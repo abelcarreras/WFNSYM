27dbd203d4984ad9708242cd711789865a15b17e
abel <abelcarreras83@gmail.com>
2018-09-04 16:19:27 +0200

    First commit

---
+++ include/wfnsym.h
@@ -0,0 +1,10 @@
+#if 0
+****************************************************
+*       David Casanova          20/OCT/11          *
+*                                                  *
+*  This file declares some environment varibles    *
+****************************************************
+#endif
+
+c #define DEVELOPMENT
+
