6cc37a85b254b84c1cc38ca3e1ed8c4ce517ab98
abel <abelcarreras83@gmail.com>
2019-02-06 17:28:25 +0100

    Added pip deployment to travis script

--- python/setup.py
+++ python/setup.py
@@ -1,27 +1,40 @@
 from numpy.distutils.core import setup, Extension
+from distutils.dir_util import copy_tree
+from distutils.errors import DistutilsFileError
+import os
 
+# Make python package
+
+
+travis = bool('TRAVIS' in os.environ)
+if travis:
+    print('Testing in travis')
+    copy_tree('../src', './src', update=True)
+    copy_tree('../include', './include', update=True)
+
+s_dir = 'src/'
 
 wfnsymlib = Extension('wfnsympy.WFNSYMLIB',
                       # extra_compile_args=['-std=c99'],
                       #include_dirs=include_dirs_numpy,
-                      include_dirs=['../include'],
+                      include_dirs=['include'],
                       libraries=['lapack', 'blas'],
                       sources=['WFNSYMLIB.pyf',
-                               '../src/VRoutines.F',
-                               '../src/aos_product.F',
-                               '../src/get_basis_lib.F',
-                               '../src/get_dim.F',
-                               '../src/get_mos.F',
-                               '../src/group_dim.F',
-                               '../src/group_table.F',
-                               '../src/make_molden.F',
-                               '../src/make_s.F',
-                               '../src/make_uhfno.F',
-                               '../src/print_routines.F',
-                               '../src/lib_main.F',
-                               '../src/overlap.F',
-                               '../src/read_routines.F',
-                               '../src/sym_routines.F'])
+                               s_dir + 'VRoutines.F',
+                               s_dir + 'aos_product.F',
+                               s_dir + 'get_basis_lib.F',
+                               s_dir + 'get_dim.F',
+                               s_dir + 'get_mos.F',
+                               s_dir + 'group_dim.F',
+                               s_dir + 'group_table.F',
+                               s_dir + 'make_molden.F',
+                               s_dir + 'make_s.F',
+                               s_dir + 'make_uhfno.F',
+                               s_dir + 'print_routines.F',
+                               s_dir + 'lib_main.F',
+                               s_dir + 'overlap.F',
+                               s_dir + 'read_routines.F',
+                               s_dir + 'sym_routines.F'])
 
 setup(name='wfnsympy',
       version='0.1',
