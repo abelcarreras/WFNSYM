2d327d6e8974f29514f8a7cf5935bbec5f10cd99
efrem <komuisan@gmail.com>
2020-06-22 17:39:47 +0200

    updated the density function to a python function to clean some lines in the code also added the spin density

--- examples/python_api.py
+++ examples/python_api.py
@@ -5,20 +5,21 @@ import numpy as np
 # data = get_data_from_file_fchk('pirrol.in.fchk')
 data = get_data_from_file_fchk('methane.fchk')
 
-pirrol = WfnSympy(coordinates=data['coordinates'],
-                  symbols=data['symbols'],
-                  basis=data['basis'],
-                  alpha_mo_coeff=data['mo_coefficients']['alpha'],
-                  # center=[0., 0., 0.],
-                  # VAxis=[0., 0., 1.],
-                  # VAxis2=[0, 1, 0],
-                  charge=0,
-                  multiplicity=1,
-                  group='Td')
+structure = WfnSympy(coordinates=data['coordinates'],
+                     symbols=data['symbols'],
+                     basis=data['basis'],
+                     alpha_mo_coeff=data['mo_coefficients']['alpha'],
+                     # center=[0., 0., 0.],
+                     # VAxis=[0., 0., 1.],
+                     # VAxis2=[0, 1, 0],
+                     charge=0,
+                     multiplicity=1,
+                     group='Td')
 
 
-pirrol.print_CSM()
-pirrol.print_overlap_mo_alpha()
-print('axis: ', pirrol.axis, np.linalg.norm(pirrol.axis))
-print('axis2: ', pirrol.axis2, np.linalg.norm(pirrol.axis2))
-print('center: ', pirrol.center)
+structure.print_CSM()
+structure.print_overlap_mo_alpha()
+structure.print_dens_CSM()
+print('axis: ', structure.axis, np.linalg.norm(structure.axis))
+print('axis2: ', structure.axis2, np.linalg.norm(structure.axis2))
+print('center: ', structure.center)
